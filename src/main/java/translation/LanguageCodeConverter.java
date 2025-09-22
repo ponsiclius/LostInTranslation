4951f67298517fc04f3e1e4a5069645f216da98f
ponsiclius <davidparent64@gmail.com>
2025-09-22 14:10:23 -0400

    src/

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,7 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                String[] split = line.split("  ");
+                String[] split = line.split("\t");
                 languageCodeToLanguage.put(split[1], split[0]);
                 languageToLanguageCode.put(split[0], split[1]);
             }
@@ -58,7 +58,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageCodeToLanguage.get(code);
     }
 
@@ -68,7 +67,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageToLanguageCode.get(language);
     }
 
